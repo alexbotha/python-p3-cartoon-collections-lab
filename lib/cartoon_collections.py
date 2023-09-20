0f536d1c511c4269fa4fd9c9ea48f1398ff6d53e
alexbotha <alexbotha@hotmail.co.uk>
2023-09-20 11:51:48 +0100

    done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,25 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(names):
+    for i in range(len(names)):
+        print(f'{i + 1}. {names[i]}')
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(planeteer_calls):
+       new = [i.title() + "!" for i in planeteer_calls]
+       return new
+       
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(words):
+    for word in words:
+         if len(word) > 4:
+              return True 
+         
+    return False
+         
+types = ["camembert", "gouda", "cheddar"]
 
-def find_the_cheese():
-    pass
+def find_the_cheese(foods):
+    
+    for food in foods:
+        if food in types:
+            return food
+
+    return None
